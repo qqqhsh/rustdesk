cf6592c46b742de85a98be27f1f797657022a3f4
qqqhsh <5098996@qq.com>
2024-08-27 12:25:59 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,9 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
-
+pub const RENDEZVOUS_SERVERS: &[&str] = &["219.148.136.54"];
+pub const PUBLIC_RS_PUB_KEY: &str = "3Jq+bC7dNPB3qHodOMWS5oRzZ5tHesaXYp6NK9KoMf0=";
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
     _ => PUBLIC_RS_PUB_KEY,
